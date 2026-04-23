adf8411074f45235f7a876952d4e4fb71a9de7a4
divya2005r <rdivya2205@gmail.com>
2026-04-24 00:00:48 +0530

    Add VariablesDemo class to demonstrate variable usage

---
+++ VariablesDemo.java
@@ -0,0 +1,11 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 21;
+        float height = 5.6f;
+        char grade = 'A';
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+    }
+}
